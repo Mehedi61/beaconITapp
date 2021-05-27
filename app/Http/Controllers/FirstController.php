41465e8625900982d16e0088816133ae1874a212
merajjahir <meraj.jahir17@gmail.com>
2021-05-27 22:12:04 +0600

    the method userName was missing the famous (this) key-word

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -7,13 +7,18 @@ use Illuminate\Http\Request;
 class FirstController extends Controller
 {
 
-    public function userName($name) {
+
+    public function userName($name) 
+    {
         return $name;
     }
 
     public function sayHello() {
 
-        $myName = userName("Mehedi");
-        echo "Hello " . $myName;
+    	//the this key-word was missing form the method
+        $myName = $this-> userName("Mehedi");
+
+        echo "Hello ".$myName ;    
+
     }
 }
